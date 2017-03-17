03a2a51a58349affaae4ffd5a2bf2bc2a4d88c0f
Valijon <parliament888@gmail.com>
2017-03-18 00:52:08 +0800

    A user my respond a thread + tests

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 Auth::routes();
 
+Route::get('', function () {
+    return view('welcome');
+});
+
 Route::get('/threads', 'ThreadsController@index');
 
 Route::get('/threads/{thread}', 'ThreadsController@show');
+
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
