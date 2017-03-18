55f441468ce2c604cf6dc6b6d744549bc5558cc1
Valijon <parliament888@gmail.com>
2017-03-18 23:50:38 +0800

    Replay Threads Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea id="body" name="body" class="form-control" placeholder="Have something to say" rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate!</p>
+        @endif
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 Auth::routes();
 
-Route::get('', function () {
+Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index');
 
 Route::get('/threads', 'ThreadsController@index');
 
