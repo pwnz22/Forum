f8a066b3ca00c25f1f2bd00985c850d566b96bb0
Valijon <parliament888@gmail.com>
2017-03-25 17:23:33 +0800

    Filtering threads by channel + tests

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('threads', 'ThreadsController@index');
+Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
-Route::post('threads', 'ThreadsController@store');
+Route::post('/threads', 'ThreadsController@store');
+Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
