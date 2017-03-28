3af031c857692cba778804f0ceec22513339c6ae
Valijon <parliament888@gmail.com>
2017-03-28 21:36:44 +0800

    Validations errors and old()

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -12,6 +12,7 @@ class ThreadsController extends Controller
     {
         $this->middleware('auth')->except(['index', 'show']);
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -41,7 +42,7 @@ class ThreadsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -51,6 +52,7 @@ class ThreadsController extends Controller
             'body' => 'required',
             'channel_id' => 'required|exists:channels,id'
         ]);
+
         $thread = Thread::create([
             'user_id' => auth()->id(),
             'channel_id' => request('channel_id'),
@@ -64,7 +66,7 @@ class ThreadsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Thread  $thread
+     * @param  \App\Thread $thread
      * @return \Illuminate\Http\Response
      */
     public function show($channelId, Thread $thread)
@@ -75,7 +77,7 @@ class ThreadsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Thread  $thread
+     * @param  \App\Thread $thread
      * @return \Illuminate\Http\Response
      */
     public function edit(Thread $thread)
@@ -86,8 +88,8 @@ class ThreadsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Thread  $thread
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Thread $thread
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Thread $thread)
@@ -98,7 +100,7 @@ class ThreadsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Thread  $thread
+     * @param  \App\Thread $thread
      * @return \Illuminate\Http\Response
      */
     public function destroy(Thread $thread)
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,16 +12,41 @@
                             {{ csrf_field() }}
 
                             <div class="form-group">
+                                <label for="channel_id">Choose a channel</label>
+                                <select name="channel_id" id="channel_id" class="form-control">
+                                    <option value="">Choose one...</option>
+                                    @foreach(App\Channel::all() as $channel)
+                                        <option value="{{ $channel->id }}">{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" name="title" id="title" placeholder="title">
+                                <input type="text" class="form-control" name="title" id="title" placeholder="title"
+                                       value="{{ old('title') }}">
                             </div>
 
                             <div class="form-group">
                                 <label for="body">Body</label>
-                                <textarea rows="5" id="body" class="form-control" name="body"></textarea>
+                                <textarea rows="5" id="body" class="form-control"
+                                          name="body">{{ old('body') }}</textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary pull-right">Publish</button>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary pull-right">Publish</button>
+                            </div>
+
                         </form>
+
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+
                     </div>
                 </div>
             </div>
