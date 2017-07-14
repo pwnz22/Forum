622cd7f3f24d00aa5a364141135dfdba6b9a11ff
Valijon <parliament888@gmail.com>
2017-07-15 03:35:38 +0800

    Activity Deletion Bug

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -73,6 +74,8 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+
+        $this->assertEquals(0, Activity::count());
     }
 
     /** @test */
