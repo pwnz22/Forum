5f176635bea13df6fdd02cc9de389dba03da805a
Valijon <parliament888@gmail.com>
2017-11-01 21:09:22 +0500

    Spam detection part2

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,16 +2,30 @@
 
 namespace Tests\Feature;
 
-use App\Spam;
+use App\Inspections\Spam;
 use Tests\TestCase;
 
 class SpamTest extends TestCase
 {
     /** @test */
-    function it_validates_spam()
+    function it_checks_for_invalid_keywords()
     {
         $spam = new Spam();
 
         $this->assertFalse($spam->detect('Innocent reply here'));
+
+        $this->expectException('Exception');
+
+        $spam->detect('yahoo customer support');
+    }
+
+    /** @test */
+    function it_checks_for_any_been_held_down()
+    {
+        $spam = new Spam();
+
+        $this->expectException('Exception');
+
+        $spam->detect('Hello World aaaaaaaaaa');
     }
 }
