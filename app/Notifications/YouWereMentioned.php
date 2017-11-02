91fce742c23fecb642728fa97e3c6b793d4f2026
Valijon <parliament888@gmail.com>
2017-11-02 12:14:18 +0500

    Mentioned User Notification Part 1 Green

---
+++ app/Notifications/YouWereMentioned.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class YouWereMentioned extends Notification
+{
+    use Queueable;
+
+    protected $reply;
+
+    public function __construct($reply)
+    {
+        $this->reply = $reply;
+    }
+
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => $this->reply->owner->name . ' mentioned you in ' . $this->reply->thread->title,
+            'link' => $this->reply->path()
+        ];
+    }
+}
