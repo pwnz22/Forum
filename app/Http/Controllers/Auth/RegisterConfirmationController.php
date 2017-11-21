2e11317a36ce031a2e30081cc95e81b04b319eea
Valijon <parliament888@gmail.com>
2017-11-22 01:17:39 +0500

    Email Confirmation Loose Ends

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
@@ -9,15 +9,14 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        } catch (\Exception $e) {
+        $user = User::where('confirmation_token', request('token'))->first();
+
+        if (!$user) {
             return redirect(route('threads'))
                 ->with('flash', 'Unknown token.');
         }
 
+        $user->confirm();
 
         return redirect(route('threads'))
             ->with('flash', 'Your account is now confirmed. You may post...');
