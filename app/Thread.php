6fe64e68a3b73d224f337063e906d50a9b2d2417
Valijon <parliament888@gmail.com>
2018-03-27 02:03:15 +0500

    First class search p1 A Review

--- app/Thread.php
+++ app/Thread.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Events\ThreadReceivedNewReply;
+use Laravel\Scout\Searchable;
 
 class Thread extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, Searchable;
 
     protected $guarded = [];
 
