6e2dc3d5af60571a792cfc2842b32c8b969e006d
Valijon <parliament888@gmail.com>
2018-03-27 01:32:58 +0500

    Threads Can Be Updated AJAX

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -1,21 +1,21 @@
 <div class="panel panel-default" v-if="editing">
     <div class="panel-heading">
         <div class="level">
-            <input type="text" value="{{ $thread->title }}" class="form-control">
+            <input type="text" value="{{ $thread->title }}" class="form-control" v-model="form.title">
         </div>
     </div>
 
     <div class="panel-body">
         <div class="form-group">
-            <textarea class="form-control" rows="5">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="5" v-model="form.body">{{ $thread->body }}</textarea>
         </div>
     </div>
 
     <div class="panel-footer">
         <div class="level">
             <button class="btn btn-xs level-item" @click="editing = true" v-show="!editing">Edit</button>
-            <button class="btn btn-primary btn-xs level-item" @click="">Update</button>
-            <button class="btn btn-xs level-item" @click="editing = false">Cancel</button>
+            <button class="btn btn-primary btn-xs level-item" @click="update">Update</button>
+            <button class="btn btn-xs level-item" @click="resetForm">Cancel</button>
 
             @can ('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-a">
@@ -36,16 +36,14 @@
                  height="25" class="mr-1">
 
             <span class="flex">
-                <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted: {{ $thread->title }}
+                <a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted: <span v-text="title"></span>
             </span>
         </div>
     </div>
 
-    <div class="panel-body">
-        {{ $thread->body }}
-    </div>
+    <div class="panel-body" v-text="body"></div>
 
-    <div class="panel-footer">
+    <div class="panel-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-xs" @click="editing = true">Edit</button>
     </div>
 </div>
